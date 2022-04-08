d5d891cebbba57d5afaf762988eb22ce7794ae24
aidancjones <97634617+aidancjones@users.noreply.github.com>
2022-04-08 11:52:00 -0600

    add java

--- scripts/assignment12.js
+++ scripts/assignment12.js
@@ -13,6 +13,27 @@ async function getQuote() {
     const json = await response.json();
     console.log(json);
     displayQuote(json[0].quote);
+    displayAuthor(json[0].author);
+  } catch(err) {
+    console.log(err)
+    alert('Failed');
+  }
+}
+
+function displayQuote(quote) {
+  const quoteText = document.querySelector('#js-quote-text');
+  quoteText.textContent = quote;
+}
+
+async function getAuthor() {
+  console.log("quote button was clicked");
+  try {
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+      throw Error(resoonse.statusText)
+    }
+    const json = await response.json();
+    console.log(json);
   } catch(err) {
     console.log(err)
     alert('Failed');
@@ -20,12 +41,14 @@ async function getQuote() {
 }
 
 
-function displayQuote(quote) {
-  const quoteText = document.querySelector('#js-quote-text');
-  quoteText.textContent = quote;
+function displayAuthor(author) {
+  const quoteText = document.querySelector('#js-author-text');
+  quoteText.textContent = author;
 }
+
 
 const endpoint = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes';
 
 const quoteButton = document.querySelector('#js-new-quote');
+quoteButton.addEventListener('click', getAuthor);
 quoteButton.addEventListener('click', getQuote);
